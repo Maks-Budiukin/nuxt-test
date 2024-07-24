340ae5d9cfab2fadedea5bafad3d878f7776e303
Max Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2024-07-25 02:08:46 +0300

    projects meta added

--- pages/[id].vue
+++ pages/[id].vue
@@ -16,9 +16,38 @@ const route = useRoute()
 
 const { data } = await useApi(route.fullPath)
 
-
 const rolesData = computed(() => {
   return data?.attributes?.myRoles
+});
+
+const seoTitle = computed(() => {
+  return `${data.attributes?.pageHeader} - Maksym Budiukin`
+})
+
+const seoDescription = computed(() => {
+  return data.attributes?.pageDescription
+})
+
+const seoImage = computed(() => {
+  return `https://test-strapi-mrqj.onrender.com${data.attributes?.gallery.data[2].attributes.formats.large.url}`
+})
+
+useHead({
+  title: seoTitle.value,
+})
+
+useSeoMeta({
+  description: seoDescription.value,
+  ogUrl: 'https://nuxt-test-sigma-dusky.vercel.app/',
+  ogTitle: seoTitle.value,
+  ogType: 'website',
+  ogDescription: seoDescription.value,
+  ogImage: {
+    url: seoImage.value,
+    width: '1200',
+    height: '630'
+  },
+  ogSiteName: 'MAKS-BUDIUKIN.COM'
 });
 
 </script>
