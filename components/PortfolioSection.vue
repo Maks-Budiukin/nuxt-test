31b14695853f6f9383a01abf4fdbca2de2116a29
Max Budiukin <106489459+Maks-Budiukin@users.noreply.github.com>
2024-07-24 14:27:46 +0300

    portfolio preview  photo width

--- components/PortfolioSection.vue
+++ components/PortfolioSection.vue
@@ -10,7 +10,7 @@
                 <div v-for="site in data" :key="site.id" class="mx-auto">
                     <div class="col-span-1 portfolio-thumb relative cursor-pointer">
                         <NuxtImg :src="`https://test-strapi-mrqj.onrender.com${site.attributes.preview.data.attributes.url}`"
-                            alt="My work-1" class="rounded-lg portfolio-picture h-full" />
+                            alt="My work-1" class="rounded-lg portfolio-picture w-full h-full" />
                         <div
                             class="button-container absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] hidden">
                             <NuxtLink
